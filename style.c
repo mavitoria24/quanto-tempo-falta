c974270d568a1759e0f313c1b16cfc017e61dc91
maria <107408180+mavitoria24@users.noreply.github.com>
2024-04-29 12:32:44 -0300

    a

--- style.c
+++ style.c
@@ -1,1 +1,14 @@
 @import url('https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@400;700&display=swap');
+:root{
+    --cor-dde-fundo:#1E1E1E;
+    --branco:#FFFFF;
+    --verde:#6FFF57;
+}
+body{
+    background-color:var(--cor-de-fundo);
+    color:vr(--branco);
+    text-align:center;
+    font-family:'Chakra Petch';
+}
+
+h1 pan{}
